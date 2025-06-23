84760ec3e500d741d4801811b96e053b9b985a4b
Tony Shi <tonyshi@stanford.edu>
2025-06-23 18:50:01 -0400

    v4

--- elevenlabs_TTS_test/main.py
+++ elevenlabs_TTS_test/main.py
@@ -10,7 +10,7 @@ elevenlabs = ElevenLabs(
 )
 
 audio = elevenlabs.text_to_speech.convert(
-    text="The first move is what sets everything in motion.",
+    text="Hi! I'm Kaia, your personal assistant. I'm here to help you with your daily tasks and make your life easier. What would you like to do?",
     voice_id="JBFqnCBsd6RMkjVDRZzb",
     model_id="eleven_multilingual_v2",
     output_format="mp3_44100_128",
